977b8a4222562fcb52044247b97a5a4101f03689
Bassel Bakr <bassel.bakr@gmail.com>
2017-10-25 21:24:12 +0200

    Update and rename KMP_Z.cpp to KMP_Z_Manacher.cpp

--- Strings/KMP_Z_Manacher.cpp
+++ Strings/KMP_Z_Manacher.cpp
@@ -3,16 +3,16 @@
 #include <cassert>
 #include <algorithm>
 using namespace std;
- 
+
 int main() {
 	string pattern, text;
 	cin >> pattern >> text;
- 
+	
 	string s = pattern + "$" + text;
- 
+	
 	int n = s.size();
 	int m = pattern.size();
- 
+	
 	// kmp
 	vector<int> k(n);
 	for(int i = 1, j = 0; i < n; ++i) {
@@ -20,7 +20,7 @@ int main() {
 			j = k[j-1];
 		k[i] = (j += (s[i] == s[j]));
 	}
- 
+	
 	// z
 	vector<int> z(n);
 	for(int i = 1, l = 0, r = 0; i < n; ++i) {
@@ -32,11 +32,30 @@ int main() {
 		if(i + len > r)
 			l = i, r = i + len;
 	}
- 
+	
+	// manacher's
+	string p(2*n+1, '*');
+	for(int i = 0; i < n; ++i)
+		p[2*i+1] = s[i];
+	n = 2*n+1;
+	vector<int> man(n);
+	for(int i = 0, c = 0, r = 0; i < n; ++i) {
+		if(i < r)
+			man[i] = min(man[c - (i - c)], r-i);
+		int& rad = man[i];
+		while(0 <= i - rad && i + rad < n && p[i-rad] == p[i+rad])
+			rad++;
+		if(i + rad > r)
+			c = i, r = i + rad;
+	}
+	
 	int k_cnt = count(k.begin(), k.end(), m);
 	int z_cnt = count(z.begin(), z.end(), m);
- 
+	int longest_palindrome = *max_element(man.begin(), man.end()) - 1;
+	
 	assert(k_cnt == z_cnt);
- 
+	assert(longest_palindrome == 9);
+	
 	cout << k_cnt << " VS " << z_cnt << endl;
+	cout << "Longest palindrome length = " << longest_palindrome << endl;
 }
